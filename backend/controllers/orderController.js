a8e39677f5d1bb386f5043187312b4bed3faab1d
pamika thanyacharoen <opera.pay@gmail.com>
2024-08-28 17:15:29 +0700

    api for fetch orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -83,4 +83,15 @@ const userOrder = async (req,res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrder}
+// Listing orders for admin panel
+const listOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({})
+        res.json({success:true, data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:'Error'})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrder, listOrders}
